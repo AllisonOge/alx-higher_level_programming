5bb2b32035da903ee31458cf7f795a95d2fe7584
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-06 19:52:28 -0700

    task 8 print string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(s):
+    upper = ""
+    for c in s:
+        if ord(c) in range(97, 123):
+            upper += "{:c}".format(ord(c) - 32)
+        else:
+            upper += c
+    print(upper)

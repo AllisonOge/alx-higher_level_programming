82b3b8b0c5c5b44a1b77c3ab24fd70d4c6f62d35
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-06 18:54:14 -0700

    task 5 satisfying requirements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,4 @@ for i in range(100):
     if i == 99:
         print(f"{i:d}")
     else:
-        if i < 10:
-            print(f"0{i:d},", end=" ")
-        else:
-            print(f"{i:d},", end=" ")
+        print(f"{i:02d},", end=" ")

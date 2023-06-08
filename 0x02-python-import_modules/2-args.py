23ae2f25ef22cfe25b3400c997ef0bc2d5832862
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-08 16:03:35 -0700

    task 2 satisfying pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     n_args = len(sys.argv) - 1
     print("{} argument{}".format(n_args, '' if n_args == 1 else 's'), end="")
-    print("{}".format('.' if  n_args == 0 else ':'))
+    print("{}".format('.' if n_args == 0 else ':'))
     for i, arg in enumerate(sys.argv[1:]):
         print("{}: {}".format(i + 1, arg))

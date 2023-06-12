fe44bc48bd87cd17045d4030cebcb7cc20c1b5f6
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-12 13:42:54 -0700

    task 6 print matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, col in enumerate(row):
+            if i == 0:
+                print("{:d}".format(col), end="")
+            else:
+                print(" {:d}".format(col), end="")
+        print()

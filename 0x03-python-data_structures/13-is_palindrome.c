a53bcf0800894effd9cca43453556bba96f5ba60
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-12 20:17:14 -0700

    task 13 check if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks if a singly linked list is palindrome
+ * @head: pointer to the head of listint_t
+ *
+ * Return: 1 if palindrome otherwise 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head; /* pointer to fill the stack */
+	int stack[1024], i = 0; /* declare the stack */
+
+	/* populate the stack with the singly linked list */
+	while (slow != NULL)
+	{
+		stack[i] = slow->n;
+		slow = slow->next;
+	}
+
+	/* iterate again over the list and check if palindrome */
+	while (*head != NULL)
+	{
+		if ((*head)->n != stack[i])
+			return (0);
+		/* increment node pointer*/
+		*head = (*head)->next;
+		/*decrement the stack index*/
+		i--;
+	}
+	return (1);
+}

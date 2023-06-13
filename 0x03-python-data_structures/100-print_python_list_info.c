a0f0881c46f15a2e9007511d0987bf958900fc9a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-13 05:11:35 +0000

    task 14 code refactoring

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,7 +2,6 @@
 
 void print_python_list_info(PyObject *p)
 {
-	PyObject *item;
 	/* get size and allocated field of the list*/
 	Py_ssize_t size = PyList_Size(p), i;
 	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
@@ -12,7 +11,7 @@ void print_python_list_info(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
+		PyObject *item = PyList_GetItem(p, i);
 		const char *typeName = Py_TYPE(item)->tp_name;
 
 		printf("Element %ld: %s\n", i, typeName);

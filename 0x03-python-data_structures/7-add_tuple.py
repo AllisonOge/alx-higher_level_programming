157d8b3c9ebcc344f1ea0b1846e4bce6616a0a46
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-12 23:01:39 -0700

    task 7 finally resolved all issues 🔥💯

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(tuple_a[i] + tuple_b[i] if i < len(tuple_a) 
-            and i < len(tuple_b) else (tuple_a[i] if i < len(tuple_a) else
-                tuple_b[i]) for i in range(2))
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
+    return tuple(tuple_a[i] + tuple_b[i] if i < len(tuple_a)
+                 and i < len(tuple_b)
+                 else (tuple_a[i] if i < len(tuple_a)
+                 else (tuple_b[i] if i < len(tuple_b) else 0))
+                 for i in range(2))

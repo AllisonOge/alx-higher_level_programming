92b8410a65743d57d167abef899388579e6ce14c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-14 21:43:23 -0700

    task 13 weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    total_weights = sum([w for _, w in my_list])
+    return sum([v * w for v, w in my_list])/total_weights if len(my_list) > 0 else 0

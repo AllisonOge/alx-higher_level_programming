9e08067d55f224b6cdcd8c552a4835f033b7b1d5
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-16 16:15:46 -0700

    task 6 reverted to past code and handled print statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for k, v in dict(sorted(a_dictionary.items(), key=lambda i: i[0])).items():
-        print("{}: ".format(k), v)
+    for k, v in dict(sorted(a_dictionary.items())).items():
+        print("{}:".format(k), v)

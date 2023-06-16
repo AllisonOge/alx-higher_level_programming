52daae1998a1f2026245d1416c5165a7c6cfd8fc
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-16 16:41:02 -0700

    task 10 added an edge case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,9 @@
 
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None
+        return
+    if len(a_dictionary) <= 0:
+        return
     best_score = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if value == best_score:

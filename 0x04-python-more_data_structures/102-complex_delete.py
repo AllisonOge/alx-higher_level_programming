47e26095e4db99e6ddda1356455e0b68b2ed2fce
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-16 17:07:19 -0700

    task 15 complex delete in a dictionary + edge case on task 6

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    if a_dictionary is None:
+        return
+    if value in a_dictionary.values():
+        del_keys = []
+        for k, v in a_dictionary.items():
+            if v == value:
+                del_keys.append(k)
+        for k in del_keys:
+            del a_dictionary[k]
+        return a_dictionary.copy()
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     for k, v in dict(sorted(a_dictionary.items())).items():
         print("{}:".format(k), v)

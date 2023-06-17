3f0e9e34741383450023237c13ecbd15ab843e51
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-16 17:25:36 -0700

    task 15 fixed code to return irrespective

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -10,4 +10,4 @@ def complex_delete(a_dictionary, value):
                 del_keys.append(k)
         for k in del_keys:
             del a_dictionary[k]
-        return a_dictionary.copy()
+    return a_dictionary.copy()

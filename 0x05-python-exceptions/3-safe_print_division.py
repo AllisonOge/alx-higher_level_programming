bd42fedf6e10ed7a634fd4b3b854bfba6ff21ba0
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-26 14:05:22 -0700

    task 3 used integer function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     result = None
     try:
-        result = a / b
+        result = int(a) / int(b)
         print("Inside result:", end=" ")
     except ZeroDivisionError:
         print("Inside result:", end=" ")

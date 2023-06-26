cc741958f9d3b1719d55696e9d6b84a6f42cd496
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-26 13:47:10 -0700

    task 1 added one more edge case

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,12 @@
 
 def safe_print_integer(value):
     try:
-        int_value = int(value)
-        print("{:d}".format(int_value))
-        return True
-    except ValueError:
+        _ = iter(value)
         return False
+    except TypeError:
+        try:
+            int_value = int(value)
+            print("{:d}".format(int_value))
+            return True
+        except ValueError:
+            return False

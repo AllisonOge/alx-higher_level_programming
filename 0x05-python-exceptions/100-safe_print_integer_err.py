7f6c5e6b3bf70a42d173e0251e02f4d07dac4e18
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-26 17:15:37 -0700

    task 7 added edge case

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,14 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(int(value)))
-        return True
+        _ = iter(value)
+        raise ValueError
+    except TypeError:
+        try:
+            print("{:d}".format(int(value)))
+            return True
+        except ValueError as err:
+            print("Exception: {}".format(err), file=sys.stderr)
+            return False
     except ValueError as err:
-        print("Exception: ", err, file=sys.stderr)
-        return False
+        print("Exception: {}".format(err), file=sys.stderr)

d214c2921db24577fd5103c8101d7ce76c23f1c1
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-27 16:16:15 -0700

    task 3 compute the area of the instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Module that defines a class with private attribute size"""
+
+
+class Square:
+    """This class is a square."""
+    def __init__(self, size=0):
+        """Initialize a Square object.
+
+        Args:
+            size (integer): the length of the square's sides
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Computes the area of the square object
+
+        Returns: area of square object
+        """
+        return (self.__size ** 2)

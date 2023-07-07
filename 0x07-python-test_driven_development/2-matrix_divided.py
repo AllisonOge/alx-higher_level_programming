942fdca9575e49cb02ed5f9472c9ce20d8144164
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-07-07 23:28:57 +0000

    task 2 satisfied pycodestyle and added some more tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,9 +23,12 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all(isinstance(e, int) or isinstance(e, float) for e in
-            [i for row in matrix for i in row])):
+                    [i for row in matrix for i in row])):
         raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
+    max_dim = max([len(row) for row in matrix])
+    if not all([len(row) == max_dim for row in matrix]):
+        raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")

6b35e04516c6f9926a560534c71e39c509ceded9
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-10 20:19:54 -0700

    task 10 create a subclass of rectangle class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module to create a subclass of the rectangle class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This is a subclass of the rectangle class"""
+    def __init__(self, size):
+        """Constructor function"""
+        super().__init__(size, size)
+        self.__size = size

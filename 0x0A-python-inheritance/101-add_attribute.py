8e13cf7198a2d2c810d3b0df937ab4e12cbe5598
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-10 20:58:19 -0700

    task 13 adding attribute to an object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module to add a new attribute to an object if possible
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Add a new attribute if possible
+
+    Args:
+        obj (object): instance of a class
+        name (string): name of attribute
+        value (any): value of attribute
+
+    Raises:
+        TypeError: if object can't have anew attribute
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't ad new attribute")
+    setattr(obj, name, value)

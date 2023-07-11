1880c0f08decc59b69fa6dd8e20c1b6cbd606f7d
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-10 18:12:59 -0700

    task 1 sort elements in ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to print the content of a list object in ascending order
+"""
+
+
+class MyList(list):
+    """This is a child class of the list object"""
+    def print_sorted(self):
+        """prints the list in ascending order"""
+        self.sort()
+        print(self)

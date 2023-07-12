dd7a722a6ab76065be81d3e42309d847226ee56a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-11 17:37:09 -0700

    task 0 read a text file with utf-8 encoding

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module to read a utf-8 encoding text file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints to stdout
+
+    Args:
+        filename (string): absolute or relative path to file
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())

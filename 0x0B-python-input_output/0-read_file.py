3e8d5739ad0536e751097693be3d97b2a2a8f92f
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-11 17:56:22 -0700

    task 0 bug fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
         filename (string): absolute or relative path to file
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")

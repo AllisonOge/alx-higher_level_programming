89b4f391973596b30a6e1f2ed21e121ed049ea78
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-11 19:57:16 -0700

    task 13 first attempt at problem

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module to inserts a new string after a certian substring in text file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific
+    string
+
+    Args:
+        filename (string): filename or path to file
+        search_string (string): search substring
+        new_string (string): text to insert
+    """
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        lines  = f.readlines()
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                print("match found")
+                f.write(new_string)
+        f.truncate()

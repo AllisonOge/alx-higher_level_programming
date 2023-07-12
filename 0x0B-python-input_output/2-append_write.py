8ad537900953eb38451799b521cd5133b1add465
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-11 18:08:10 -0700

    task 2 append to a text file with utf-8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-module to write to a text file using utf-8 encoding
+module to write append to a text file using utf-8 encoding
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
-    Write a string to a text file with utf-8 encoding
-    and return the number of number of characters written
+    Appends a string at the end of a text file encoded with utf-8
+    and returns the number of characters added
 
     Args:
         filename (string): filename or path to file (relative or absolute)
@@ -17,6 +17,6 @@ def write_file(filename="", text=""):
         (integer): number of characters written
     """
     nchars = 0
-    with open(filename, mode="w", encoding="utf-8") as f:
+    with open(filename, mode="a", encoding="utf-8") as f:
         nchars = f.write(text)
     return nchars

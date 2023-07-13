822b292b7c1d8d42cce88bf766a4982ae3e9e8f9
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-12 17:21:19 -0700

    task 7, 13 satisfied pycodestyle

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -15,11 +15,10 @@ def append_after(filename="", search_string="", new_string=""):
         new_string (string): text to insert
     """
     with open(filename, mode="r+", encoding="utf-8") as f:
-        lines  = f.readlines()
+        lines = f.readlines()
         f.seek(0)
         for line in lines:
             f.write(line)
             if search_string in line:
                 f.write(new_string)
         f.truncate()
-
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """
 module for adding all arguments to a python list and saves them to a file
 """
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":

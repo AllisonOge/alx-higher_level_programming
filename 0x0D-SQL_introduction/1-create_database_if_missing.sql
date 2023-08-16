67f0cd7f2e87b5f200a069a1fe83bc4bd97ccaa9
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-08-15 18:45:00 -0700

    task 2 remove database if exists and corrected name in task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- a script that creates a database hbtn_0c_0 if it does not exist
-create database if not exists hntb_0c_0;
+create database if not exists hbtn_0c_0;;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- a script that removes the database hbtn_0c_0 if exists
+drop database if exists hbtn_0c_0;

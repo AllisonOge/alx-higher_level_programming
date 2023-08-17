bb70efd2bcd0578c63b3342f1493bfbaad295ed3
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-08-17 04:44:56 +0000

    advanced tasks attempt

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- script that list all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT tv_shows.title AS title
+FROM tv_shows JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name != "Comedy"
+ORDER BY title;

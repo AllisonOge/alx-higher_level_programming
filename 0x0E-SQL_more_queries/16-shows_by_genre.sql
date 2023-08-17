44c8543a026c6973e9b557f048f324c2e4d7c653
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-08-17 14:40:25 -0700

    task 16 corrected sql statement

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that list all shows and all genres linked to that show
 SELECT tv_shows.title AS title, tv_genres.name AS name
-FROM tv_shows JOIN tv_show_genres
+FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, name;

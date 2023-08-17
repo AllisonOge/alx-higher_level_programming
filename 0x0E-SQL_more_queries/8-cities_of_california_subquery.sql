72d1b0f0fa4d0b4c8dc7b794741e6ef85f809dda
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-08-17 04:29:18 +0000

    code formatting and corrections

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- a script that lists all the cities of california that can be found in the database hbtn_0d_usa
 -- You are not allowed to use JOIN statement
-SELECT id, name FROM cities WHERE id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+SELECT id, name FROM cities WHERE id = (SELECT id FROM states WHERE name = "California") ORDER BY id;

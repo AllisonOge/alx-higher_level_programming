b809a14176ee595c8e4c9b90ac8db25df19dfbe4
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-09-11 16:29:45 -0700

    task 3 attempt at satisfying all requirements

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) console.log('No argument');
+if (process.argv[2] === undefined) console.log('No argument');
 else {
   for (let idx = 2; process.argv[idx] !== undefined; idx++) {
     console.log(process.argv[idx]);

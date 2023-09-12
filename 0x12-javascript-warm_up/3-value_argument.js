ac5e800b47d01e08dca03d66cfceeed88ab610ab
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-09-12 01:21:52 +0000

    task 3 attempt to resolve errors

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) console.log('No argument');
 else {
-  for (let idx = 2; process.argv[idx] !== undefined; idx++) {
-    console.log(process.argv[idx]);
-  }
+  console.log(process.argv[2]);
 }

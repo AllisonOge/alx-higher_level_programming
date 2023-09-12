21f53d34f32bb75c268248f4da53a4292b41534e
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-09-11 17:26:34 -0700

    task 11 second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = [];
+for (let i = 2; i < process.argv.length; i++) {
+  argv.push(parseInt(process.argv[i]));
+}
+if (argv.length === 0 || argv.length === 1) console.log(0);
+else {
+  console.log(argv.sort()[argv.length - 2]);
+}

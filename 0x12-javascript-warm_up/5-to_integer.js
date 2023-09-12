628d2b51d74e3e30c62d9a031362a49e08aa9db5
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-09-12 01:19:01 +0000

    task 5 attempt to fix errors

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const number = process.argv[2] || undefined;
+const number = process.argv[2];
 if (isNaN(parseInt(number))) {
   console.log('Not a number');
-} else console.log('My number: ', parseInt(number));
+} else console.log(`My number: ${parseInt(number)}`);

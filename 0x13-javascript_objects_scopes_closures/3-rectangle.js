1fc262c302da35e010c09adae86fa5c6a006e875
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-09-12 17:14:38 -0700

    task 3 code fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
         row += 'X';

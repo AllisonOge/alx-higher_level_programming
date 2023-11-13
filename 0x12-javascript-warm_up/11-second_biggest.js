9ca44ae47ba54fb041e3c3823df142693301b56f
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-11-13 21:44:19 +0000

    satisfying semistandard style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const argv = [];
 for (let i = 2; i < process.argv.length; i++) {
   argv.push(parseInt(process.argv[i]));
 }
-if (argv.length === 0 || argv.length === 1)  console.log(0);
+if (argv.length === 0 || argv.length === 1) console.log(0);
 else {
   console.log(argv.sort()[argv.length - 2]);
 }

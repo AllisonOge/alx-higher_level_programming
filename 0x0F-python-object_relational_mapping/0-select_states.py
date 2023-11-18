317c7b72d5ed26c5008e82d26086159aa34d5419
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-17 17:58:16 -0800

    task#0 pycodestyle requirement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()

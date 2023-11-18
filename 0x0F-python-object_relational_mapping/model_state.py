5e5bf98cdd0bb6e792b4d477807c29353e436f35
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-18 15:23:59 -0800

    task#6 meeting requirements

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,5 +10,5 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = "states"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)

da3bccb5d43602b6085432e4b81300cf23eaeacc
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-17 18:13:31 -0800

    task#1 filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for filtering database querying using MySQLdb
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=user,
+                           passwd=passwd, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states
+                   WHERE name LIKE 'N%'
+                   ORDER BY id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

2079f488941f0ee0fdd8d377c2c3e3ac92a06621
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-19 15:58:42 -0800

    task#10 get a state using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module for quering database for a record usign SQLAlchemy
+"""
+import sys
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    query_result = session.query(State)\
+                          .filter_by(name = sys.argv[4])\
+                          .order_by(State.id).first()
+    if not query_result:
+        print("Not found")
+    else:
+        print("{}".format(query_result.id))
+    session.close()

143ef8c5696a16435ec08aa935a3cc4e885336b4
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-19 19:05:20 -0800

    task#2 made it case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                            db=db_name, charset="utf8")
     cur = conn.cursor()
     cur.execute("""SELECT * from states
-                   WHERE name='{}'
+                   WHERE name LIKE BINARY '{}'
                    ORDER BY id ASC""".format(st_name))
     query_rows = cur.fetchall()
     for row in query_rows:

625a01cc86a2d20e97f73db460614cd00a47bd68
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-11-19 19:14:07 -0800

    task#16 replaced \t with 4 spaces

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,6 +4,7 @@ module for related objects using SQLAlchemy
 """
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy.orm import Session
 
 from sqlalchemy import create_engine
@@ -18,5 +19,5 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
     session.close()

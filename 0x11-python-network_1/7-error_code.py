18b1aef45d975f483a684501670617f656559656
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-12-02 03:09:42 +0000

    task#7 handling error code with requests lib

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Take in a URL, send a request to the URL and display the body of the
+response (decoded in utf-8).
+If the HTTP status code is greater than or equal to 400, print: Error code:
+followed by the value of the HTTP status code
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        with requests.get(sys.argv[1]) as response:
+            print(response.text)
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.response.status_code))

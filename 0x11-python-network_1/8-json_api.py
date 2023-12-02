08ae9dbb86930e7b2d7ef90ecbc7118efac5577c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-12-01 19:22:37 -0800

    task#8 search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    res = requests.post("http://0.0.0.0:5000/search_user", { "q": letter})
+    try:
+        res_data = res.json()
+        if len(res_data) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(res_data.get("id"), res_data.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

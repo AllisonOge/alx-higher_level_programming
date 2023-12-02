acbe4208adbc960b7fb0cd15b4d9b97349de8f2b
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-12-02 01:56:03 +0000

    task#1 fetches the X-Request-Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches a URL and displays the value of the X-Request-Id variable"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1])\
+            as response:
+        print(response.headers.get("X-Request-Id"))

5de889402c0038c364e19b928942acd5dbb173db
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-12-01 19:05:54 -0800

    task#6 post an email with requests lib

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Takes a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.post(sys.argv[1], {"email": sys.argv[2]})
+    print(res.text)

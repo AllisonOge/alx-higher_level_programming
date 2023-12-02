8d214fd08236c6fc6f9b2b94f459b5869ccf156e
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-12-01 18:53:36 -0800

    task#4 taks requirements

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
-import sys
 import requests
 
 
 if __name__ == "__main__":
     res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("    - type: {}".format(type(res.text)))
+    print("    - content: {}".format(res.text))

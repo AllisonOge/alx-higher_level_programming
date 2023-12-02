0a5b4c8a37f0ff1c894098a1fc080f6b2016faf2
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-12-02 04:22:29 +0000

    task#8 pycodestyle requirement

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import requests
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    res = requests.post("http://0.0.0.0:5000/search_user", { "q": letter})
+    res = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
     try:
         res_data = res.json()
         if len(res_data) == 0:
